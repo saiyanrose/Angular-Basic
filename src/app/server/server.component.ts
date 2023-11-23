a88bc7b88e70d376b306ae81b208eab15edf20d2
saiyanrose <piyushmalik08@gmail.com>
2023-11-23 12:40:52 +0530

    component lifecycles, custom data binding and events ng-content viewchild contentviewchild

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { AfterContentInit, DoCheck, EventEmitter, Input, OnDestroy, Output, SimpleChanges } from "@angular/core";
+import { Component, OnChanges, OnInit } from "@angular/core";
 
 @Component({
     selector:'app-server',
@@ -9,20 +10,58 @@ import { Component } from "@angular/core";
         }
     `]
 })
-export class ServerComponent{
-    //string interpolation
+export class ServerComponent implements OnInit,OnChanges,DoCheck,AfterContentInit,OnDestroy{    
     serverId:number=10;
     serverStatus:string='offline';
+    
+    @Input()allservers:[serverName:string];
 
-    getServerStatus(){
-        return this.serverStatus;
-    }
+    @Output()getServerDetails=new EventEmitter<{serverId:number,serverStatus:string}>();
 
     constructor(){
+        console.log('constructor called');                        
         this.serverStatus=Math.random() >0.5 ? 'online' : 'offline';
     }
 
+    ngOnDestroy(): void {
+        console.log('ngOnDestroy called');
+    }    
+
+    ngAfterContentInit(): void {
+        console.log('ngAfterContentInit called');
+    }
+    
+    ngDoCheck(): void {
+        console.log('ngDoCheck called');
+    }
+
+    ngOnInit(): void {
+        console.log('ngOnInit called');
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('ngOnChanges called: '+changes);
+    }
+
+    //@Output()
+    onCreateServer(){
+        this.getServerDetails.emit({serverId:this.serverId,serverStatus:this.serverStatus});
+    }
+
+    //@Input()
+    getServerStatus(){
+        console.log('servers from servers component to server component: '+this.allservers);
+        return this.serverStatus;
+    }    
+
+    //ngStyle
     getColor(){
         return this.serverStatus==='online' ? 'green' : 'red';
     }
+
+    onDestroyFirst(){
+        console.log(this.allservers);
+        this.allservers.splice(0,1);        
+    }
+   
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 
 @Component({
@@ -15,22 +15,38 @@ export class ServersComponent{
   allowNewServer:boolean=false;
   serverCreationStatus='no server was created';
   serverName="";
-  serverCreated=false;
-  servers=['test','test1'];
+  serverCreated=false;  
+  servers=[];
 
+  @ViewChild('serverNameContent')serverNameContent:ElementRef;
+
+  //disable button for 2sec
   constructor(){
     setTimeout(()=>{
       this.allowNewServer=true;
     },2000);
-  }
+  }  
+  
+  onCreateServer(serverData:{serverId:number,serverStatus:string}){
+    //@Output
+    console.log('serverId and serverStatus: '+serverData.serverId+' '+serverData.serverStatus);    
 
-  onCreateServer(){
+    //show @viewChild()
+    console.log('@viewChild(): '+this.serverNameContent.nativeElement.value);
+    
+    //set serverCreated to true
     this.serverCreated=true;
+
+    //push into servers array
     this.servers.push(this.serverName);
+
+    //set server info
     this.serverCreationStatus='server is created name: '+this.serverName;
   }
 
+  //add server name
   onUpdateServerName(event:any){
     this.serverName=event.target.value;    
   }
+  
 }
