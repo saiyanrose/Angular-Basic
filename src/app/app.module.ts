b0d81c7e7e5422ec3d57b24e0eb8a2487dfed906
saiyanrose <piyushmalik08@gmail.com>
2023-11-24 13:37:13 +0530

    ngSwitch,ngIf,ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { FormsModule } from '@angular/forms';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { OddevenComponent } from './oddeven/oddeven.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    OddevenComponent
   ],
   imports: [
     BrowserModule,FormsModule
---
+++ src/app/oddeven/oddeven.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oddeven',
+  templateUrl: './oddeven.component.html',
+  styleUrls: ['./oddeven.component.css']
+})
+export class OddevenComponent implements OnInit {
+  numbers=[1,2,3,4,5]
+  odd=[1,3,5]
+  even=[2,4]
+  onlyOdd=false
+  value=5
+
+  constructor() { } 
+
+  ngOnInit() {
+  }
+
+}
