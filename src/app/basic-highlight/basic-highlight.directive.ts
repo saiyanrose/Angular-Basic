cfc30c1afedd068d4316ab389bb261a941a76156
saiyanrose <piyushmalik08@gmail.com>
2023-11-24 12:18:38 +0530

    hostbinding and hostlistener

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,15 +1,27 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector:'[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit{
 
+    @HostBinding('style.backgroundColor')backgroundColor:string='transparent';
+
     //constructor(private elementRef:ElementRef){}
     constructor(private elementRef:ElementRef,private renderer:Renderer2){}
 
     ngOnInit(): void {
        // this.elementRef.nativeElement.style.backgroundColor='green';
-       this.renderer.setStyle(this.elementRef.nativeElement,'background-color','blue');
+       //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','blue');
+    }
+
+    @HostListener('mouseenter') mouseover(eventData:Event){
+        //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','blue');
+        this.backgroundColor='blue';
+    }
+
+    @HostListener('mouseleave') mouseleave(eventData:Event){
+        //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','transparent');
+        this.backgroundColor='transparent';        
     }
 }
